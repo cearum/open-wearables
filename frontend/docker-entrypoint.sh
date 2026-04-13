55b85681c99affc0d5fede6f64298a201f2de1c4
cearum <33786368+cearum@users.noreply.github.com>
2026-04-13 12:09:20 -0400

    Update docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 
 if [ -n "$VITE_API_URL" ]; then
-  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__VITE_API_URL_PLACEHOLDER__|${VITE_API_URL}|g" {} +
+  find /usr/share/nginx/html -type f -name '*.js' -exec sed -i "s|__VITE_API_URL_PLACEHOLDER__|${VITE_API_URL}|g" {} + 2>/dev/null || true
+  find .output -type f -name '*.mjs' -o -name '*.js' | xargs sed -i "s|__VITE_API_URL_PLACEHOLDER__|${VITE_API_URL}|g" 2>/dev/null || true
 fi
 
 exec "$@"
